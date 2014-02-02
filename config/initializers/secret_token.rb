ca05483b46beba90183e535ba3151a98fcaa1843
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2014-02-01 19:42:12 -0500

    initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FunfeedApp::Application.config.secret_key_base = 'e48dc28ee08cbe59e4c33184d799d93a3a2fd955e2551de69299131b191570887e998699e6a62fb04d75c486115783ffc3b5dde5fe599dee25c1b7e5d6cfd6a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FunfeedApp::Application.config.session_store :cookie_store, key: '_funfeed_app_session'
